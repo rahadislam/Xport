0cc6845a92db524eb04c74b8e28b3d6a914a2a9d
rahadislam <rahadislam.web@gamil.com>
2023-09-29 17:10:54 +0600

    content update

--- src/pages/Home/Project.js
+++ src/pages/Home/Project.js
@@ -16,19 +16,19 @@ const Project = () => {
 
                     <div className='flex items-center gap-2'>
                     <AiFillCheckCircle className='text-green-400'></AiFillCheckCircle> 
-                    <p className='font-medium text-xs lg:text-sm'> By using this website you can buy!</p>
+                    <p className='font-medium text-xs lg:text-sm'> You can add order & Update</p>
                     </div>
                     <div className='flex items-center gap-2'>
                     <AiFillCheckCircle className='text-green-400'></AiFillCheckCircle> 
-                    <p className='font-medium text-xs lg:text-sm'> By using this website you can buy!</p>
+                    <p className='font-medium text-xs lg:text-sm'> Contorl user & set admin Power</p>
                     </div>
                     <div className='flex items-center gap-2'>
                     <AiFillCheckCircle className='text-green-400'></AiFillCheckCircle> 
-                    <p className='font-medium text-xs lg:text-sm'> By using this website you can buy!</p>
+                    <p className='font-medium text-xs lg:text-sm'> By using this website Review & post blog!</p>
                     </div>
                     <div className='flex items-center gap-2'>
                     <AiFillCheckCircle className='text-green-400'></AiFillCheckCircle> 
-                    <p className='font-medium text-xs lg:text-sm'> By using this website you can buy!</p>
+                    <p className='font-medium text-xs lg:text-sm'>You can see active user</p>
                     </div>
 
                     <div class="card-actions mt-5 justify-center">
@@ -42,19 +42,19 @@ const Project = () => {
                     <h2 class="font-container text-xl text-center ">Organic Foods</h2>
                     <div className='flex items-center gap-2'>
                     <AiFillCheckCircle className='text-green-400'></AiFillCheckCircle> 
-                    <p className='font-medium text-xs lg:text-sm'> By using this website you can buy!</p>
+                    <p className='font-medium text-xs lg:text-sm'>This  website,You can manage Product list</p>
                     </div>
                     <div className='flex items-center gap-2'>
                     <AiFillCheckCircle className='text-green-400'></AiFillCheckCircle> 
-                    <p className='font-medium text-xs lg:text-sm'> By using this website you can buy!</p>
+                    <p className='font-medium text-xs lg:text-sm'>You can Product add,remove,update</p>
                     </div>
                     <div className='flex items-center gap-2'>
                     <AiFillCheckCircle className='text-green-400'></AiFillCheckCircle> 
-                    <p className='font-medium text-xs lg:text-sm'> By using this website you can buy!</p>
+                    <p className='font-medium text-xs lg:text-sm'>User login and logout system!</p>
                     </div>
                     <div className='flex items-center gap-2'>
                     <AiFillCheckCircle className='text-green-400'></AiFillCheckCircle> 
-                    <p className='font-medium text-xs lg:text-sm'> By using this website you can buy!</p>
+                    <p className='font-medium text-xs lg:text-sm'> User can review product</p>
                     </div>
                     <div class="card-actions mt-5 justify-center">
                         <Link to='/project2' class="btn btn-xs sm:btn-sm md:btn-md lg:btn-md text-xs bg-black border-transparent text-white font-container">Details</Link>
@@ -67,19 +67,19 @@ const Project = () => {
                     <h2 class="font-container text-xl text-center ">Career Coach</h2>
                     <div className='flex items-center gap-2'>
                     <AiFillCheckCircle className='text-green-400'></AiFillCheckCircle> 
-                    <p className='font-medium text-xs lg:text-sm'> By using this website you can buy!</p>
+                    <p className='font-medium text-xs lg:text-sm'>User apoinmment system implement </p>
                     </div>
                     <div className='flex items-center gap-2'>
                     <AiFillCheckCircle className='text-green-400'></AiFillCheckCircle> 
-                    <p className='font-medium text-xs lg:text-sm'> By using this website you can buy!</p>
+                    <p className='font-medium text-xs lg:text-sm'>authorized user system implement</p>
                     </div>
                     <div className='flex items-center gap-2'>
                     <AiFillCheckCircle className='text-green-400'></AiFillCheckCircle> 
-                    <p className='font-medium text-xs lg:text-sm'> By using this website you can buy!</p>
+                    <p className='font-medium text-xs lg:text-sm'> Blog post and review system</p>
                     </div>
                     <div className='flex items-center gap-2'>
                     <AiFillCheckCircle className='text-green-400'></AiFillCheckCircle> 
-                    <p className='font-medium text-xs lg:text-sm'> By using this website you can buy!</p>
+                    <p className='font-medium text-xs lg:text-sm'> Cheak out system </p>
                     </div>
                     <div class="card-actions mt-5 justify-center">
                         <Link to='/project3' class="btn btn-xs sm:btn-sm md:btn-md lg:btn-md text-xs bg-black border-transparent text-white font-container">Details</Link>
